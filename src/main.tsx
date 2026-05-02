24f9068471932b71ac0418e383adefe7fb39bf01
ashcolor <ashcolor27@gmail.com>
2026-05-02 19:48:47 +0900

    add: サイト設定

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => undefined);
+  });
+}
+
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <App />
